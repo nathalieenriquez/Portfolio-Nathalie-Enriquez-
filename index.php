a7ec3c10c33bd6a68fc90df26ac2dcdf59d1c4c1
Nathalie Enriquez <youremail@example.com>
2024-02-01 21:46:09 +0800

    updated

--- index.php
+++ index.php
@@ -1,24 +1,4 @@
 <?php
-    echo"OMG! <br>";
-    echo"Wews! HAHAHAHA <br>";
-    // This is a comment
-    /* Multiline comment
-    hehe
-    */
+    
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <button>order pizza</button>
-    <button>order pizza</button>
-    <button>order pizza</button>
-    <button>order pizza</button>
-    <button>order pizza</button>
-</body>
-</html>
