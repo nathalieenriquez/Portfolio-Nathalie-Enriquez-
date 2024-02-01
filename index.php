6090c8d37d8631dcfa2abb1182cf16fce390aaf4
Nathalie Enriquez <youremail@example.com>
2024-02-01 21:12:08 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+    echo"OMG! <br>";
+    echo"Wews! HAHAHAHA <br>";
+    // This is a comment
+    /* Multiline comment
+    hehe
+    */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <button>order pizza</button>
+</body>
+</html>
